0c3ffb36b7fbd9867d050c7fd123443d22c6d24c
LuncyBloont <xhymaker@126.com>
2024-05-10 01:09:49 +0800

    init

---
+++ init.lua
@@ -0,0 +1,26 @@
+vim.cmd("colorscheme desert")
+
+local Plug = vim.fn["plug#"]
+
+vim.call("plug#begin")
+Plug("neoclide/coc.nvim", {["branch"] = "release"})
+vim.call("plug#end")
+
+require("coc-init")
+
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.colorcolumn = "120"
+vim.opt.textwidth = 120
+vim.opt.wrap = true
+vim.opt.sidescrolloff = 30
+vim.opt.scrolloff = 12
+vim.opt.sidescroll = 1
+vim.opt.scroll = 1
+vim.opt.linespace = 5
+
+
